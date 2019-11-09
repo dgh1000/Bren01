372acd257237fcfc29c4aec07bcbed2aef958289
Bren Goforth <hergeekness@me.com>
2019-11-08 18:55:30 -0800

    First day's work on js

---
+++ src/first.js
@@ -0,0 +1,6 @@
+console.log("Now this!");
+console.log("Hello!");
+console.log("Kenny ROCKS!");
+console.log("blah blah");
+console.log("Mike ROCKS!");
+console.log("Nikki ROCKS MORE!");
---
+++ src/varfun.js
@@ -0,0 +1,6 @@
+r = "a new thing called a string here";
+console.log (typeof r);
+nikkit = ["elmo", "blue ball", "raccoon"];
+whichToy = nikkit[1];
+console.log (typeof nikkit);
+console.log (whichToy);
